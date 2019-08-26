f206494e725ac0d360740a5cc7324822deccba07
ben <benjivou2@gmail.com>
2019-08-26 19:46:09 +0200

    Add a filemanager object

--- src/test/Test_UiInput.java
+++ src/test/Test_UiInput.java
@@ -1,9 +1,13 @@
 package test;
 
+import serialize.FileManager;
 import ui.inputActivity.FrameInput;
 
+import java.io.IOException;
+
 public class Test_UiInput {
-	public static void main(String[] args) {
-		FrameInput fi = new FrameInput();
+	public static void main(String[] args) throws IOException {
+        FileManager fm = new FileManager("./tutu.txt");
+	    FrameInput fi = new FrameInput(fm);
 	}
 }
